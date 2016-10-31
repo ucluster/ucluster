9739158de8c52b0d878895b0ed0d13fb3e156cef
kiwi <jcwen@thoughtworks.com>
2016-10-31 13:47:32 +0800

    remove not used property in user_dsl.js for test

--- mongo-api/src/test/resources/user_dsl.js
+++ mongo-api/src/test/resources/user_dsl.js
@@ -18,12 +18,6 @@ user({
         email: true,
         identity: true
     },
-    id_number: {
-        format: {
-            pattern: "^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}([0-9]|X)$"
-        }
-    },
-    id_name: {},
     nickname: {
         format: {
             pattern: "\\w{6,16}"
